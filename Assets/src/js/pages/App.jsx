0718d9e28e1c11408b07c674e8f68d8daea8ea4c
nuintun <nuintun@qq.com>
2019-03-26 15:53:32 +0800

    :tada: Initial commit

--- Assets/src/js/pages/App.jsx
+++ Assets/src/js/pages/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Button } from 'antd';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <Button>SUBMIT</Button>;
+    return <Button>Button</Button>;
   }
 }
 
